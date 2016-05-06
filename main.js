5021d2573cc6ec62d21932631c16855daedaf1a8
Kite <ixkaito@gmail.com>
2016-05-07 08:17:02 +0900

    Add score and time labels

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ window.onload = function() {
   core.preload('chara1.png');
   core.fps = 15;
 
+  var score = 0;
+  var timeLeft = 5 * core.fps;
+
   core.onload = function() {
 
     var bear = new Sprite(32, 32);
@@ -14,7 +17,33 @@ window.onload = function() {
     bear.frame = 0;
     bear.image = core.assets['chara1.png'];
 
+    bear.on('touchstart', function() {
+      score++;
+      scoreLabel.text = 'Score: ' + score;
+      this.x = rand(320);
+      this.y = rand(320);
+    });
+
+    var scoreLabel = new Label('Score: 0');
+    scoreLabel.x = 200;
+    scoreLabel.y = 5;
+
+    var timeLabel = new Label('Time: ?');
+    timeLabel.x = 5;
+    timeLabel.y = 5;
+
+    core.on('enterframe', function() {
+      timeLeft--;
+      timeLabel.text = 'Time: ' + timeLeft;
+      if (timeLeft <= 0) {
+        alert('Your score: ' + score);
+        this.stop();
+      }
+    });
+
     core.rootScene.addChild(bear);
+    core.rootScene.addChild(scoreLabel);
+    core.rootScene.addChild(timeLabel);
 
   };
 
